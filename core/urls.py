9e3dec00bb704639fd78df4a9c6b56df3b5669f5
Edineusa Gomes <edineusamariagomes@gmail.com>
2025-04-03 21:18:42 -0300

    feat login

--- core/urls.py
+++ core/urls.py
@@ -3,14 +3,19 @@ from django.urls import path
 from produtos.views import ProdutoListView, ProdutoCreateView, ProdutoUpdateView, ProdutoDeleteView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', ProdutoListView.as_view(), name='produtos-lista' ),
+
     path('produto/add/', ProdutoCreateView.as_view(), name='produto-create'),
     path('produto/update/<int:pk>', ProdutoUpdateView.as_view(), name='produto-update'),
     path('produto/delete/<int:pk>', ProdutoDeleteView.as_view(), name='produto-delete'),
+
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
 
 ]
 
